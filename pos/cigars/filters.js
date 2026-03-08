7c6134e99efc328db315b46d2586ab969ce2e9c9
cigarmoses <me@michaelmoses.com>
2026-03-08 19:53:52 -0400

    Create filters.js

---
+++ pos/cigars/filters.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-filters").onclick=()=>{
+
+document.getElementById("filters-modal").style.display="block"
+
+}
+
+document.getElementById("filters-close").onclick=()=>{
+
+document.getElementById("filters-modal").style.display="none"
+
+}
